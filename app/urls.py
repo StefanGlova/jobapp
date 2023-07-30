539fa9df23b06e0c938c5f560fa41da942f79043
StefanGlova <glovastefan@protonmail.com>
2023-07-30 23:28:01 +0100

    new update, urls.py for app + 2 views

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from app.views import hello, job_detail
+from app import views
+
+
+urlpatterns = [
+    path('', views.hello),
+    path('job/<int:id>', views.job_detail),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+job_title = [
+    "First Job",
+    "Second Job"
+]
+
+job_description = [
+    "First job description",
+    "Second job description"
+]
+
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello World")
+
+def job_detail(request, id):
+    return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
+    return HttpResponse(return_html)
