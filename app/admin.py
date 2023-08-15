04bf03df2ab5ee31b7dc6d8314c9b75d3519130c
StefanGlova <glovastefan@protonmail.com>
2023-08-15 22:20:01 +0100

    new filed added to model + admin superuser set up

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app.models import JobPost
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'title', 'salary', 'date')
 
 # Register your models here.
+admin.site.register(JobPost, JobAdmin)
