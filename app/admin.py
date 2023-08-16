1313c888a41bc2037eb6b4379704ffa8d449c861
StefanGlova <glovastefan@protonmail.com>
2023-08-16 05:42:13 +0100

    customised admin pannel

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,26 @@ from app.models import JobPost
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'title', 'salary', 'date')
+    list_filter = ('date', 'salary', 'expiry')
+    search_fields = ('title', 'description')
+    search_help_text = "Help"
+    # fields = (('title', 'description'), 'expiry')
+    # exclude = ('title',)
+    fieldsets = (
+        ('Basic information', {
+            "fields": (
+                'title',
+                'description'
+            ),
+        }),
+        ('More information', {
+            "classes": ('collapse',),
+            "fields": (
+                ('expiry', 'salary'),
+                'slug'
+            )
+        })
+    )
 
 # Register your models here.
 admin.site.register(JobPost, JobAdmin)
