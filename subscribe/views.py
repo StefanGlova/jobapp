d374a3be8851fa23c8db2670e5889cff4b05a67d
StefanGlova <glovastefan@protonmail.com>
2023-09-13 21:55:10 +0100

    subscribe model created and migrated to database

--- subscribe/views.py
+++ subscribe/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def subscribe(request):
-    context = {}
+    email_error_empty=""
+    if request.POST:
+        email = request.POST["email"]
+        if email=="":
+            email_error_empty="No email entered"
+    context = {"email_error_empty": email_error_empty}
     return render(request, 'subscribe/subscribe.html', context)
