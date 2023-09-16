a6acb64611b8cf2ec5088020538ca1fd4a3e51e0
StefanGlova <glovastefan@protonmail.com>
2023-09-16 21:57:33 +0100

    forms updated with hlep_text

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -2,28 +2,26 @@ from django import forms
 from subscribe.models import Subscribe
 from django.utils.translation import gettext_lazy as _
 
+
 class SubscribeForm(forms.ModelForm):
     class Meta:
-        model=Subscribe
-        fields="__all__" # can be added as list if not all are required ["first_name", "email"]
-        labels={
-            "first_name":_("Enter First Name: "),
-            "last_name":_("Enter Last Name: "),
-            "email":_("Enter Email: ")
+        model = Subscribe
+        fields = "__all__"  # can be added as list if not all are required ["first_name", "email"]. Also using exclude=("first_name") which will put all fields, excluding listed
+        labels = {
+            "first_name": _("Enter First Name: "),
+            "last_name": _("Enter Last Name: "),
+            "email": _("Enter Email: "),
         }
-        error_messages={
-            "first_name": {
-                "required": _("You cannot move forward without first name")
-            }
+        help_text = {"first_name": _("Enter Characters Only")}
+        error_messages = {
+            "first_name": {"required": _("You cannot move forward without first name")}
         }
-
-
 
     # def validate_comma(value):
     #     if "," in value:
     #         raise forms.ValidationError("Invalid Last Name")
     #     return value
-    # 
+    #
     # first_name = forms.CharField(
     #     max_length=100,
     #     required=False,
@@ -40,4 +38,3 @@ class SubscribeForm(forms.ModelForm):
     #     max_length=100,
     #     label="Enter Email"
     # )
-
